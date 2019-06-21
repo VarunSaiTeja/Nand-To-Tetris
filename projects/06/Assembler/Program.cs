d8c31011aa0ec00873f290dbd360bfc16656c860
VarunSaiTeja <35555010+VarunSaiTeja@users.noreply.github.com>
2019-06-21 13:14:54 +0530

    Assembler to read all lines from ASM file

--- projects/06/Assembler/Program.cs
+++ projects/06/Assembler/Program.cs
@@ -7,6 +7,7 @@ namespace Assembler
 {
     class Program
     {
+        static List<string> Lines = new List<string>();
         static void Main(string[] args)
         {
             string asm_name;
@@ -20,7 +21,19 @@ namespace Assembler
             {
                 asm_name = args[0];
             }
+
+            Get_All_Lines(asm_name);
             Console.ReadLine();
+        }
+
+        static void Get_All_Lines(string asm_name)
+        {
+            StreamReader asm_file = new StreamReader(asm_name);
+            while (!asm_file.EndOfStream)
+            {
+                Lines.Add(asm_file.ReadLine().Trim());
+            }
+            asm_file.Close();
         }
     }
 }
