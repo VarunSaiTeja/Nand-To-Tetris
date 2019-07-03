820bee95ec654a57e0bbdf1b08ccafd6c012ce4b
VarunSaiTeja <35555010+VarunSaiTeja@users.noreply.github.com>
2019-07-03 19:31:36 +0530

    VM Translator just added

---
+++ projects/07/VM Translator/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace VM_Translator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
