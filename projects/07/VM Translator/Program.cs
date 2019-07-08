23e7df620b550579ef13e3281504c93ee5e6ccea
VarunSaiTeja <35555010+VarunSaiTeja@users.noreply.github.com>
2019-07-08 17:41:15 +0530

    VM Translator Extention Updated

--- projects/07/VM Translator/Program.cs
+++ projects/07/VM Translator/Program.cs
@@ -274,7 +274,7 @@ namespace VM_Translator
         static void Generate_Assembly()
         {
             StreamReader vm_file = new StreamReader(vm_name);
-            StreamWriter asm_file = new StreamWriter(vm_name.Substring(0, vm_name.LastIndexOf(".")) + ".asm");
+            StreamWriter asm_file = new StreamWriter(Path.ChangeExtension(vm_name,"asm"));
             string temp = null;
             while (!vm_file.EndOfStream)
             {
