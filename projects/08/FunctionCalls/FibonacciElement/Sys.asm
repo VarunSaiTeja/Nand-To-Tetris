e959dba3cf31e0d4eceacfa275b397f9da25f417
VarunSaiTeja <35555010+VarunSaiTeja@users.noreply.github.com>
2019-08-04 22:38:29 +0530

    VM Translator Final Commit

---
+++ projects/08/FunctionCalls/FibonacciElement/Sys.asm
@@ -0,0 +1,66 @@
+//function Sys.init 0
+(Sys.init)
+//push constant 4
+@4
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+//call Main.fibonacci 1
+@Return_Label_3
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@LCL
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@ARG
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@THIS
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@THAT
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@SP
+D=M
+@1
+D=D-A
+@5
+D=D-A
+@ARG
+M=D
+@SP
+D=M
+@LCL
+M=D
+@Main.fibonacci
+0;JMP
+(Return_Label_3)
+//label WHILE
+(WHILE)
+//goto WHILE
+@WHILE
+0;JMP
