7778f9ae83066e671d4b80aa4c92e3da33a93ae7
VarunSaiTeja <35555010+VarunSaiTeja@users.noreply.github.com>
2019-09-07 17:51:41 +0530

    Initial commit for Jack Analyzer

---
+++ projects/10/Jack Analyzer/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Jack_Analyzer
+{
+    class Program
+    {
+        static string jack_name;
+
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.Write("File name : ");
+                jack_name = Console.ReadLine();
+            }
+            else
+                jack_name = args[0];
+
+            Tokenizer.Tokenize(jack_name);
+            Console.ReadLine();
+        }
+    }
+
+    class Tokenizer
+    {
+        static string all_tokens = String.Empty;
+        public static void Tokenize(String Fname)
+        {
+            Remove_Garbage(Fname);
+            Console.WriteLine(all_tokens);
+        }
+
+        static void Remove_Garbage(String Fname)
+        {
+            StreamReader jack_file = new StreamReader(Fname);
+            string temp = null;
+
+            while (!jack_file.EndOfStream)
+            {
+                temp = jack_file.ReadLine().Trim();
+                if (!(temp == ""))
+                {
+                    if (!(temp.StartsWith("//")) || !(temp.Contains("/*")))
+                    {
+                        if (temp.Contains("//"))
+                        {
+                            temp = temp.Substring(0, temp.IndexOf("//"));
+                            temp = temp.Trim();
+                        }
+                        else if (temp.Contains("/*"))
+                        {
+                            temp = temp.Substring(0, temp.IndexOf("/*")).Trim();
+                        }
+                        all_tokens += temp;
+                    }
+                }
+            }
+
+            jack_file.Close();
+        }
+    }
+}
